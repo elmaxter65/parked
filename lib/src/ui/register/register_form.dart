8405e582a4228109cc0e8ea49d5582b77a511109
johan <johanuneg15@gmail.com>
2020-04-08 20:25:14 -0500

    Login, menu, metodos bloc

---
+++ lib/src/ui/register/register_form.dart
@@ -0,0 +1,183 @@
+import 'package:country_pickers/country.dart';
+import 'package:country_pickers/country_pickers.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_firebase_flutter_2/src/bloc/authentication_bloc/bloc.dart';
+import 'package:flutter_firebase_flutter_2/src/bloc/register_bloc/bloc.dart';
+import 'package:flutter_firebase_flutter_2/src/ui/register/register_button.dart';
+
+class RegisterForm extends StatefulWidget {
+  @override
+  _RegisterFormState createState() => _RegisterFormState();
+}
+
+class _RegisterFormState extends State<RegisterForm> {
+  // Dos variables
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  Country _selectedDialogCountry =
+      CountryPickerUtils.getCountryByPhoneCode('90');
+
+  RegisterBloc _registerBloc;
+
+  bool get isPopulated =>
+      _emailController.text.isNotEmpty && _passwordController.text.isNotEmpty;
+
+  bool isRegisterButtonEnabled(RegisterState state) {
+    return state.isFormValid && isPopulated && !state.isSubmitting;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _registerBloc = BlocProvider.of<RegisterBloc>(context);
+    _emailController.addListener(_onEmailChanged);
+    _passwordController.addListener(_onPasswordChanged);
+  }
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocListener<RegisterBloc, RegisterState>(
+        listener: (context, state) {
+      // Si estado es submitting
+      if (state.isSubmitting) {
+        Scaffold.of(context)
+          ..hideCurrentSnackBar()
+          ..showSnackBar(SnackBar(
+            content: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text('Registering'),
+                CircularProgressIndicator()
+              ],
+            ),
+          ));
+      }
+      // Si estado es success
+      if (state.isSuccess) {
+        BlocProvider.of<AuthenticationBloc>(context).add(LoggedIn());
+        Navigator.of(context).pop();
+      }
+      // Si estado es failure
+      if (state.isFailure) {
+        Scaffold.of(context)
+          ..hideCurrentSnackBar()
+          ..showSnackBar(SnackBar(
+            content: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text('Registration Failure'),
+                Icon(Icons.error)
+              ],
+            ),
+            backgroundColor: Colors.red,
+          ));
+      }
+    }, child: BlocBuilder<RegisterBloc, RegisterState>(
+      builder: (context, state) {
+        return Padding(
+          padding: EdgeInsets.all(20),
+          child: Form(
+            child: ListView(
+              children: <Widget>[
+                Card(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text("Seleccionar pais de origen"),
+                      ListTile(
+                        onTap: _openCountryPickerDialog,
+                        title: _buildDialogItem(_selectedDialogCountry),
+                      ),
+                    ],
+                  ),
+                ),
+                // Un textForm para email
+                TextFormField(
+                  controller: _emailController,
+                  decoration: InputDecoration(
+                    icon: Icon(Icons.email),
+                    labelText: 'Email',
+                  ),
+                  keyboardType: TextInputType.emailAddress,
+                  autocorrect: false,
+                  autovalidate: true,
+                  validator: (_) {
+                    return !state.isEmailValid ? 'Invalid Email' : null;
+                  },
+                ),
+                // Un textForm para password
+                TextFormField(
+                  controller: _passwordController,
+                  decoration: InputDecoration(
+                      icon: Icon(Icons.lock), labelText: 'Password'),
+                  obscureText: true,
+                  autocorrect: false,
+                  autovalidate: true,
+                  validator: (_) {
+                    return !state.isPasswordValid ? 'Invalid Password' : null;
+                  },
+                ),
+                // Un button
+                RegisterButton(
+                  onPressed:
+                      isRegisterButtonEnabled(state) ? _onFormSubmitted : null,
+                )
+              ],
+            ),
+          ),
+        );
+      },
+    ));
+  }
+
+  void _onEmailChanged() {
+    _registerBloc.add(EmailChanged(email: _emailController.text));
+  }
+
+  void _onPasswordChanged() {
+    _registerBloc.add(PasswordChanged(password: _passwordController.text));
+  }
+
+  void _onFormSubmitted() {
+    _registerBloc.add(Submitted(
+        email: _emailController.text, password: _passwordController.text));
+  }
+
+  Widget _buildDialogItem(Country country) => Row(
+        children: <Widget>[
+          CountryPickerUtils.getDefaultFlagImage(country),
+          SizedBox(width: 8.0),
+          Flexible(child: Text(country.name))
+        ],
+      );
+
+  void _openCountryPickerDialog() => showDialog(
+        context: context,
+        builder: (context) => Theme(
+          data: Theme.of(context).copyWith(primaryColor: Colors.pink),
+          child: CountryPickerDialog(
+            titlePadding: EdgeInsets.all(8.0),
+            searchCursorColor: Colors.pinkAccent,
+            searchInputDecoration: InputDecoration(hintText: 'Buscar...'),
+            isSearchable: true,
+            title: Text('Seleccione tu pais de origen'),
+            onValuePicked: (Country country) =>
+                setState(() => _selectedDialogCountry = country),
+            itemBuilder: _buildDialogItem,
+            priorityList: [
+              CountryPickerUtils.getCountryByIsoCode('TR'),
+              CountryPickerUtils.getCountryByIsoCode('US'),
+            ],
+          ),
+        ),
+      );
+}
